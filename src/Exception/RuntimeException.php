1f6ddf59d787ca6e11bb0ea0ac875a0f5a3c4833
lexlutorcorp <lexlutorcorp@gmail.com>
2017-05-31 18:17:54 +0200

    Update RuntimeException.php

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace SebastianBergmann\Git;
+namespace lexlutorcorp\Git;
 
 class RuntimeException extends \RuntimeException implements Exception
 {
